031bc2b6730e2c0df94cb0d5b2f2b730a0af7e9f
Subak Systems <info@subak.jp>
2013-05-16 03:08:04 +0900

    2013年 5月16日 木曜日 03時08分04秒 JST

--- 02/assets/app/application.js
+++ 02/assets/app/application.js
@@ -1,12 +1,21 @@
 define([
   "knockout", "domino-application", "domino-model",
-  "app/model/app", "app/view/app", "app/controller/app",
+  "app/view/app", "app/controller/app",
   "app/model/item", "app/view/item",
   "app/view/title",
   "app/model/form", "app/view/form"
-], function (ko, parent, Model, AppModel, AppView, AppController, ItemModel, ItemView, TitleView,
-             FormModel, FormView) {
-  function Application() {
+], function (
+  ko, parent, Model,
+  AppView, AppController,
+  ItemModel, ItemView, TitleView,
+  FormModel, FormView) {
+  "use strict";
+
+  /**
+   * @class Application
+   * @constructor
+   */
+  var self = parent.subclass(function Application() {
     parent.apply(this, arguments);
 
     var scopeModel = new Model;
@@ -18,15 +27,22 @@ define([
 
     formModel.value = {}; // render
     appView.render(); // domViewの場合は連動してdomを置き換える
-  }
+  });
 
-  var fn = parent.extends(Application);
-
-  fn.getElement = function (id) {
+  /**
+   * @method getElement
+   * @param {int} id
+   * @returns {HTMLElement}
+   */
+  self.defineMethod(function getElement (id) {
     return document.querySelector("[data-view-id='" + id + "']");
-  };
+  });
 
-  fn.createItemView = function (data) {
+  /**
+   * @method createItemView
+   * @param {Object} data
+   */
+  self.defineMethod(function createItemView(data) {
     data.items.forEach(function (data) {
       var item = new ItemModel(data);
       var _item = this.getModel(item.id);
@@ -36,7 +52,7 @@ define([
         itemView.render();
       }
     }, this);
-  };
+  });
 
-  return Application;
+  return self;
 });
